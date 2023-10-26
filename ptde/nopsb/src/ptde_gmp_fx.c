dfff84385f393e64fd173884bc2e64eef987a835
r3sus <49620111+r3sus@users.noreply.github.com>
2023-10-26 15:45:12 +0300

    PTDE DLL Kit

--- ptde/nopsb/src/ptde_gmp_fx.c
+++ ptde/nopsb/src/ptde_gmp_fx.c
@@ -1,6 +1,6 @@
 #include <windows.h>
 
-#define DEBUG
+//#define DEBUG
 
  #ifdef DEBUG
 #include <stdio.h>
@@ -13,17 +13,40 @@
 
 #include "d8w.c"
 
-byte byseq [] = { 0x8B, 0x51, 0x10, 0x6A, 0x01, 0x56 };
-//                                         ^
-void attach_hook(void)
+void gamepad()
+{
+byte b1 = 2, gamepad_sig [] = { 0x8B, 0x51, 0x10, 0x6A, 0x01, 0x56 };                                         
+void *x = sigscan(gamepad_sig, sizeof(gamepad_sig));
+wp(x+4, &b1, 1);
+}
+
+void logo()
+{
+byte logo_sig [] = {0x8B, 0x86, 0x00, 0x01, 0x00, 0x00, 0x83, 0xF8, 0x03, 0x0F, 0x87},
+     logo_fix [] = {0xe9, 0x27, 0x01, 0x00, 0x00, 0x90, 0x90,};
+void *x = sigscan(logo_sig, sizeof(logo_sig));
+wp(x+0x10, logo_fix, sizeof(logo_fix));
+}
+
+void offline()
+{
+byte offline_sig [] = {0x80, 0xBE, 0x05, 0x01, 0x00, 0x00, 0x00, 0x5F},
+     offline_fix [] = {0x74, 0x0d,};
+void *x = sigscan(offline_sig, sizeof(offline_sig));
+wp(x+0x83-0x7B, offline_fix, sizeof(offline_fix));
+}
+
+
+void attach_hook()
 {
 #ifdef DEBUG
  AllocConsole(); freopen("CONOUT$", "w", stdout);
 #endif
 
-void *x = sigscan(byseq, sizeof(byseq));
-byte b1 = 2;
-wp(x+4, &b1, 1);
+gamepad();
+logo();
+offline();
+
 }
        
 BOOL APIENTRY DllMain(HMODULE mod, DWORD reason, LPVOID res)
