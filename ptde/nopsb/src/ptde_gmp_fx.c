15737cf7557361069c0dc4a4dc67e62f1c631c58
r3sus <49620111+r3sus@users.noreply.github.com>
2023-10-27 15:30:13 +0300

    merge introoffline with gamepad (DLL)

--- ptde/nopsb/src/ptde_gmp_fx.c
+++ ptde/nopsb/src/ptde_gmp_fx.c
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include "introoffline.c"
 
 //#define DEBUG
 
@@ -20,23 +21,6 @@ void *x = sigscan(gamepad_sig, sizeof(gamepad_sig));
 wp(x+4, &b1, 1);
 }
 
-void logo()
-{
-byte logo_sig [] = {0x8B, 0x86, 0x00, 0x01, 0x00, 0x00, 0x83, 0xF8, 0x03, 0x0F, 0x87},
-     logo_fix [] = {0xe9, 0x27, 0x01, 0x00, 0x00, 0x90, 0x90,};
-void *x = sigscan(logo_sig, sizeof(logo_sig));
-wp(x+0x10, logo_fix, sizeof(logo_fix));
-}
-
-void offline()
-{
-byte offline_sig [] = {0x80, 0xBE, 0x05, 0x01, 0x00, 0x00, 0x00, 0x5F},
-     offline_fix [] = {0x74, 0x0d,};
-void *x = sigscan(offline_sig, sizeof(offline_sig));
-wp(x+0x83-0x7B, offline_fix, sizeof(offline_fix));
-}
-
-
 void attach_hook()
 {
 #ifdef DEBUG
@@ -44,9 +28,7 @@ void attach_hook()
 #endif
 
 gamepad();
-logo();
-offline();
-
+introoffline();
 }
        
 BOOL APIENTRY DllMain(HMODULE mod, DWORD reason, LPVOID res)
